d141688da82337549ca74fc2fe2f7750333bc782
julia-richards <julia.m.richards@gmail.com>
2020-10-21 17:04:25 -0600

    finshed phase 3

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const csrfProtection = csrf( { cookie: true} )
+const csrfProtection = csrf({ cookie: true });
 app.use(cookieParser());
 
-app.use(express.urlencoded( { extended: true }))
-
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
 
@@ -27,53 +26,69 @@ app.get("/", (req, res) => {
 
 app.get("/create", csrfProtection, (req, res) => {
 	res.render("create-user", {
-    title: "Create User",
-    csrfToken: req.csrfToken()
-	})
+		title: "Create User",
+		csrfToken: req.csrfToken(),
+	});
 });
 
-
 const validateUser = (req, res, next) => {
-	const { firstName, lastName, email, password, confirmedPassword } = req.body
+	const {
+		firstName,
+		lastName,
+		email,
+		password,
+		confirmedPassword,
+	} = req.body;
 	const errors = [];
 
-	if(!firstName) {
-		errors.push("Please provide a first name.")
+	if (!firstName) {
+		errors.push("Please provide a first name.");
 	}
-	if(!lastName) {
-		errors.push("Please provide a last name.")
+	if (!lastName) {
+		errors.push("Please provide a last name.");
 	}
-	if(!email) {
-		errors.push("Please provide an email.")
+	if (!email) {
+		errors.push("Please provide an email.");
 	}
-	if(!password) {
-		errors.push("Please provide a password.")
+	if (!password) {
+		errors.push("Please provide a password.");
 	}
-	if(password !== confirmedPassword) {
-		errors.push("The provided values for the password and password confirmation fields did not match.")
+	if (password !== confirmedPassword) {
+		errors.push(
+			"The provided values for the password and password confirmation fields did not match."
+		);
 	}
-	req.errors = errors
-	next()
-}
+	req.errors = errors;
+	next();
+};
 
-app.post('/create', csrfProtection, validateUser, (req, res) => {
-	const { id, firstName, lastName, email, password } = req.body
+app.post("/create", csrfProtection, validateUser, (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const id = users.length + 1
+
 	if (req.errors.length > 0) {
-		res.render("create-user", { csrfToken: req.csrfToken(), id, firstName, lastName, email, password, errors: req.errors })
-	return
-  }
-    else{
+		res.render("create-user", {
+			csrfToken: req.csrfToken(),
+			id,
+			firstName,
+			lastName,
+			email,
+			password,
+			errors: req.errors,
+		});
+		// return;
+	} else {
 		const user = {
 			id,
 			firstName,
 			lastName,
-			email
-		}
-		users.push(user)
-		res.redirect("/")
-	    }
-})
+			email,
+		};
+		users.push(user);
+		res.redirect("/");
+	}
+});
 
-	app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-	module.exports = app;
+module.exports = app;
